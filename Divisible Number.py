f414181a766cc5f7301f0fb2340e4d5ee738074f
anmice <manmice6@gmail.com>
2025-07-03 15:46:29 +0530

    Add files via upload

---
+++ Divisible Number.py
@@ -0,0 +1,8 @@
+print("enter number(numerator)")
+numn = int (input())
+print("enter number(denominator)")
+numd = int (input())
+if numn%numd == 0:
+    print("it is divisible")
+else: 
+    print ("it is not divisible")

6766fb94f099dc3a37507f9a56086bfa04dec5e1
anmice <manmice6@gmail.com>
2025-07-12 15:37:27 +0530

    Add files via upload

---
+++ Reverse a String.py
@@ -0,0 +1,5 @@
+string = input("enter the string")
+string1 = ('')
+for i in string:
+    string1 = i + string1
+print("reverse of string is ",string1)
---
+++ reverse order.py
@@ -0,0 +1,4 @@
+n =int(input("enten the value of n"))
+print(" values are ".format(n,1))
+for i in range(n,0,-1):
+    print(i)
